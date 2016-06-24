46ee6734a0cf217b766d3b50593a6efb756a0df6
RobertBowie <robertb1903@gmail.com>
2016-06-23 22:46:26 -0700

    Added save success toastr

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import courses from './courseReducer';
 import authors from './authorReducer';
 import numAjaxCallsInProgress from './ajaxStatusReducer';
 
+
 const rootReducer = combineReducers({
   courses,
   authors,
