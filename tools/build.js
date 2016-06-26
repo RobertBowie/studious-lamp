04554a58d5989b06b3c6edc958b951c3950330d4
RobertBowie <robertb1903@gmail.com>
2016-06-26 15:22:33 -0700

    Added console output during build

---
+++ tools/build.js
@@ -0,0 +1,34 @@
+// Allow console calls in build mode
+/* eslint-disable no-console */
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config.prod';
+import colors from 'colors';
+
+process.env.NODE_ENV = 'production';
+
+console.log('Generating minified bundle for production via Webpack.  This will take a moment...'.blue);
+
+webpack(webpackConfig).run((err, stats) => {
+  if (err) { // stop in the case of fatal error
+    console.log(err.bold.red);
+    return 1;
+  }
+
+  const jsonStats = stats.toJson();
+
+  if (jsonStats.hasErrors) {
+    return jsonStats.errors.map(error => console.log(error.red));
+  }
+
+  if (jsonStats.hasWarnings) {
+    console.log('Webpack generated the following warnings: '.bold.yellow);
+    jsonStats.warnings.map(warning => console.log(warning.yellow));
+  }
+
+  console.log(`Webpack stats: ${stats}`);
+
+  // build success
+  console.log('App has been successfully compiled in production mode and written to /dist.'.green);
+
+  return 0;
+});
